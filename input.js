f53110f0220288d9c0bfde3892197b577d5687b6
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-25 19:42:29 +0000

    making snake move as a result of user input

--- input.js
+++ input.js
@@ -1,7 +1,22 @@
+let connection;
+
 const handleUserInput = function(stdin) {
+  let message;
   stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+    else if (key === 's') {
+      connection.write("Move: down");
+    }
+    else if (key === 'a') {
+      connection.write("Move: left");
+    }
+    else if (key === 'd') {
+      connection.write("Move: right");
     }
   });
 };
@@ -10,12 +25,16 @@ const handleUserInput = function(stdin) {
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
- const setupInput = function () {
+ const setupInput = function (conn) {
+   connection = conn; 
    const stdin = process.stdin; 
    stdin.setRawMode(true);
    stdin.setEncoding('utf8');
    stdin.resume();
+  
   handleUserInput(stdin);
+
+  
    
    return stdin;
  }
--- play.js
+++ play.js
@@ -2,8 +2,9 @@ const {connect}  = require ('./client');
 const {setupInput} = require('./input');
 
 console.log('Connecting ...');
-connect();
+
+const conn = connect();
 
 
 
- setupInput();
+ setupInput(conn);
